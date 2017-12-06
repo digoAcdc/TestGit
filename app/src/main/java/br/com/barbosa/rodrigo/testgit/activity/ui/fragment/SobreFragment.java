c880fa17de29e59386b828b364718784088a27b7
rodrigo silveira <rodrigo.silveira@b2wdigital.com>
2017-12-06 09:44:53 -0200

    gits

--- app/src/main/java/br/com/barbosa/rodrigo/testgit/activity/ui/fragment/SobreFragment.java
+++ app/src/main/java/br/com/barbosa/rodrigo/testgit/activity/ui/fragment/SobreFragment.java
@@ -2,10 +2,13 @@ package br.com.barbosa.rodrigo.testgit.activity.ui.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import br.com.barbosa.rodrigo.testgit.R;
 
@@ -14,10 +17,7 @@ import br.com.barbosa.rodrigo.testgit.R;
  */
 public class SobreFragment extends Fragment {
 
-
-    public SobreFragment() {
-        // Required empty public constructor
-    }
+    private TextView tvSobre;
 
 
     @Override
@@ -27,4 +27,12 @@ public class SobreFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_sobre, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvSobre = (TextView)view.findViewById(R.id.tvSobre);
+
+        tvSobre.setText(Html.fromHtml(getString(R.string.sobre)));
+    }
 }
